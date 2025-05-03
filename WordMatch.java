77715a17d5f07c57dad33d379e32e29e8f8ed69d
Alessandro-netizen <alessandrom12@nycstudents.net>
2025-05-03 13:17:47 -0400

    Update WordMatch.java

--- WordMatch.java
+++ WordMatch.java
@@ -1,19 +1,12 @@
 public class WordMatch {
-    /** The secret string. */
+
     private String secret;
 
-    /**
-     * Constructs a WordMatch object with the given secret string of lowercase
-     * letters.
-     */
+
     public WordMatch(String word) {
         secret = word;
     }
 
-    /**
-     * Returns a score for guess, as described in part (a).
-     * Precondition: 0 < guess.length() <= secret.length()
-     */
     public int scoreGuess(String guess) {
         int score = 0;
         for (int i = 0; i <= secret.length() - guess.length(); i++) {
@@ -24,13 +17,7 @@ public class WordMatch {
         return score * guess.length() * guess.length();
     }
 
-    /**
-     * Returns the better of two guesses, as determined by scoreGuess and the rules
-     * for a
-     * tie-breaker that are described in part (b).
-     * Precondition: guess1 and guess2 contain all lowercase letters.
-     * guess1 is not the same as guess2.
-     */
+ 
     public String findBetterGuess(String guess1, String guess2) {
         int score1 = scoreGuess(guess1);
         int score2 = scoreGuess(guess2);
@@ -40,7 +27,6 @@ public class WordMatch {
         } else if (score2 > score1) {
             return guess2;
         } else {
-            // Tie-breaker: the alphabetically greater guess is better.
             if (guess1.compareTo(guess2) > 0) {
                 return guess1;
             } else {
